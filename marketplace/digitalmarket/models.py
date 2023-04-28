ba15580e833b344e846bfe575d31fc319bd40ba8
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-28 15:56:07 +0300

    Add Product model

--- marketplace/digitalmarket/models.py
+++ marketplace/digitalmarket/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    file = models.FileField(upload_to='uploads')
+    
+    def __str__(self):
+        return self.name

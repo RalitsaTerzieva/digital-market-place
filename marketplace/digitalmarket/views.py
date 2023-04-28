b33c1263e9488a2eace1d4a40ca38b252bf6cf8a
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-28 16:16:21 +0300

    Show products

--- marketplace/digitalmarket/views.py
+++ marketplace/digitalmarket/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import Product
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Product
     template_name = 'digitalmarket/index.html'

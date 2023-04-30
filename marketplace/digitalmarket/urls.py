da66e6b8a6993a2bfcceca453609333a4368487e
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-30 12:34:20 +0300

    Add detail view logic

--- marketplace/digitalmarket/urls.py
+++ marketplace/digitalmarket/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ProductDetailView
 
 urlpatterns = [
     path("",IndexView.as_view(),name="index"),
+    path("detail/<int:pk>",ProductDetailView.as_view(),name='detail'),
+    
 ]
--- marketplace/digitalmarket/views.py
+++ marketplace/digitalmarket/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+
+from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from .models import Product
 
 class IndexView(ListView):
     model = Product
     template_name = 'digitalmarket/index.html'
+    
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    # def get_queryset(self):
+    #     pk = self.kwargs['pk']
+    #     return Product.objects.filter(pk=pk)

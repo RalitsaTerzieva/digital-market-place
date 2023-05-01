e77e2636ee88ae6cbe31f2f8aab2f5ad4827caa3
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-01 11:56:13 +0300

    Add url for checkout session

--- marketplace/digitalmarket/urls.py
+++ marketplace/digitalmarket/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from .views import IndexView, ProductDetailView,payment_success_view,payment_failed_view
+from .views import IndexView, ProductDetailView,payment_success_view,payment_failed_view,create_checkout_session
 
 urlpatterns = [
     path("",IndexView.as_view(),name="index"),
     path("detail/<int:pk>",ProductDetailView.as_view(),name='detail'),
     path("success/",payment_success_view,name="success"),
     path("failed/",payment_failed_view,name='failed'),
-    
+    path("api/checkout-session/<int:id>/",create_checkout_session,name='api_checkout_session'),
 ]

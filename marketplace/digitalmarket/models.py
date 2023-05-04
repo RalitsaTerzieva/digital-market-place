b1bd66e09edd0974deda50481c9a2b5fe1cc2bd2
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-05-04 10:59:03 +0300

    Add new product model field

--- marketplace/digitalmarket/models.py
+++ marketplace/digitalmarket/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    seller = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     price = models.FloatField()
